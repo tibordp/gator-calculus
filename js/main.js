c9e975a2ef7c8a9422acb9aa8c6284e32d21866f
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2015-09-23 18:04:52 +0200

    Cleaned up.

--- js/main.js
+++ js/main.js
@@ -1,52 +1,19 @@
-function modalClose() {
-  $('.modal-overlay').removeClass("shown");
-  $('.modal').removeClass("shown");
-}
+var phi = 1.618033988749894848204;
 
-var color = 0;
-var zoom = 1;
-
-$("#toolbar-clear").click(function() {
-  row.element.children().remove();
-});
-
-$("#toolbar-zoomin").click(function() {
-  zoom *= 1.1;
-  if (zoom > 1) zoom = 1; // Gators don't scale well :(
-  $("board").css("transform", "translate(-50%, -50%) scale(" + String(zoom) + ")");
-});
-
-$("#toolbar-save").click(function() {
-  var blob = new Blob([JSON.stringify(row.export(), null, 2)], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, "board.json");
-});
-
-
-$("#toolbar-zoomout").click(function() {
-  zoom /= 1.1;
-  $("board").css("transform", "translate(-50%, -50%) scale(" + String(zoom) + ")");
-});
-
-function nextColor() {
-  var color = 0;
-  var colors = row.allColors();
-  while (color in colors) { color++; }
-  return color;
-}
-
-$('.modal-overlay').on("click", modalClose);
+// =============================== SPRITE =================================== //
 
 function Sprite(sprite_name, tag_name) {
   this.element = $(document.createElement(tag_name || "sprite"));
   this.element.html(Sprites[sprite_name]);
 }
 
+// ================================ GATOR =================================== //
+
 function Gator(old) {
   this.element = $(document.createElement("gator"));
   this.old = !!old;
   this.element.html(Sprites[old ? "old-gator-true.svg" : "alligator.svg"]);
   this.jaw = $(".upper_jaw", this.element);
-  var gator = this;
   this.setColor(0);
 }
 
@@ -58,22 +25,24 @@ Gator.prototype.die = function(callback) {
     gator.element.remove();
     if (callback) callback();
   }, 1500);
-}
+};
 
 Gator.prototype.openJaw = function () {
-  this.jaw.attr("class", "upper_jaw open")
-}
+  this.jaw.attr("class", "upper_jaw open");
+};
 
 Gator.prototype.closeJaw = function () {
-  this.jaw.attr("class", "upper_jaw closed")
-}
+  this.jaw.attr("class", "upper_jaw closed");
+};
 
 Gator.prototype.setColor = function(color) {
   this.color = color;
   $(".colored", this.element).css("fill", "hsl(" +
-    String(360 * ((color * 1.618033988749894848204) % 1))
-  + ", 50%, 50%)");
-}
+  String((360 * color * phi) % 360) +
+  ", 50%, 50%)");
+};
+
+// ================================= EGG ==================================== //
 
 function Egg(color) {
   this.element = $(document.createElement("egg"));
@@ -82,9 +51,9 @@ function Egg(color) {
   this.element.data("reference", this);
 }
 
-Egg.prototype.clone = function (callback) {
+Egg.prototype.clone = function () {
   return new Egg(this.color);
-}
+};
 
 Egg.prototype.hatch = function (callback) {
   var element = this.element;
@@ -94,17 +63,29 @@ Egg.prototype.hatch = function (callback) {
   window.setTimeout(function() {
     element.removeClass("shake shake-constant");
     if (callback) callback();
-  }, 3000)
-}
+  }, 3000);
+};
 
 Egg.prototype.setColor = function(color) {
   this.color = color;
   $(".colored", this.element).css("fill", "hsl(" +
-    String(360 * ((color * 1.618033988749894848204) % 1))
-   + ", 50%, 50%)");
-}
+  String((360 * color * phi) % 360) +
+  ", 50%, 50%)");
+};
 
-function Family(color, old_gator, row_callback) {
+Egg.prototype.export = function() {
+  return {
+    type: "egg", color: this.color
+  };
+};
+
+Egg.import = function(data) {
+  return new Egg(data.color);
+};
+
+// ================================ FAMILY ================================== //
+
+function Family(color, old_gator, row) {
   var family = this;
 
   this.bound_eggs = [];
@@ -126,7 +107,7 @@ function Family(color, old_gator, row_callback) {
     });
   }
 
-  this.row = row_callback ? row_callback(this) : new Row(this);
+  this.row = row || new Row();
   this.element = $(document.createElement("board-group")).addClass("col")
   .data("reference", this)
   .append(this.gator.element)
@@ -135,21 +116,20 @@ function Family(color, old_gator, row_callback) {
   .mouseover(function(event) {
     $(this).addClass("hover").parents().removeClass("hover");
     event.stopPropagation();
-  }).mouseout(function(event) {
+  }).mouseout(function() {
     $(this).removeClass("hover");
   }).click(function(event) {
     $('.modal-overlay').addClass("shown");
-    var modal = $('#family-modal')
-    .addClass("shown");
+    $('#family-modal').addClass("shown");
 
     $("#context-delete").off("click").on("click", function() {
       family.element.remove();
-      modalClose();
+      Session.modalClose();
     });
 
     $("#context-clone").off("click").on("click", function() {
       family.clone().element.insertAfter(family.element);
-      modalClose();
+      Session.modalClose();
     });
 
     event.stopPropagation();
@@ -161,32 +141,19 @@ function Family(color, old_gator, row_callback) {
 Family.prototype.dieOfAge = function() {
   var family = this;
   var eater = family.gator.element;
-  var eater_svg = $("svg", family.gator.element);
 
   var children = family.row.element.children("egg, board-group");
   if (children.size() != 1) return;
 
 
-    if (!family.canEatOrBeEaten()) return;
-    family.busy = true;
+  if (!family.canEatOrBeEaten()) return;
+  family.busy = true;
 
   eater.addClass("dying");
   family.gator.die(function() {
     family.element.replaceWith(children);
   });
-}
-
-Row.prototype.allColors  = function() {
-  var result = {};
-  if (!this.old_gator) result[this.color]  = [ this ];
-  this.element.find("board-group.col, egg").each(function() {
-    var ref = $(this).data("reference");
-    if (ref.old_gator) return;
-    result[ref.color] = true;
-  });
-
-  return result;
-}
+};
 
 Family.prototype.allColors = function() {
   var result = {};
@@ -200,43 +167,42 @@ Family.prototype.allColors = function() {
   });
 
   return result;
-}
+};
 
 Family.prototype.ancestors = function() {
   return this.element.parents("board-group.col").toArray().map(function(element) {
     return $(element).data("reference");
   });
-}
+};
 
 Family.prototype.descendants = function() {
   return this.element.find("board-group.col").toArray().map(function(element) {
     return $(element).data("reference");
   });
-}
+};
 
 Family.prototype.canEatOrBeEaten = function() {
   if (this.busy) return false;
   return this.ancestors().every(function (family) { return !family.busy; }) &&
-    this.descendants().every(function (family) { return !family.busy; });
-}
+  this.descendants().every(function (family) { return !family.busy; });
+};
 
 Family.prototype.eat = function() {
   var family = this;
   var eater = family.gator.element;
-  var eater_svg = $("svg", family.gator.element);
   var eaten = family.element.next();
   var eaten_family = eaten.data("reference");
   if (!eaten_family) return;
 
   if (!family.canEatOrBeEaten() ||
-    (eaten.is("board-group") && !eaten_family.canEatOrBeEaten()))
-    return;
+  (eaten.is("board-group") && !eaten_family.canEatOrBeEaten()))
+  return;
 
   family.busy = true;
   eaten_family.busy = true;
 
   var hasToRename = eaten.is("board-group") &&
-    Family.checkAlpha(this, eaten_family);
+  Family.checkAlpha(this, eaten_family);
 
   window.setTimeout(function() {
     var replacements = family.associatedEggs().map(function(egg) {
@@ -247,15 +213,15 @@ Family.prototype.eat = function() {
     eaten.addClass("eaten");
 
     var deltax = eater.offset().left - eaten.offset().left +
-    zoom * (eater.outerWidth() - eaten.outerWidth()) / 2;
+    Session.zoom * (eater.outerWidth() - eaten.outerWidth()) / 2;
     var deltay = eater.offset().top - eaten.offset().top +
-    zoom * (eater.outerHeight() - eaten.outerHeight()) / 2;
+    Session.zoom * (eater.outerHeight() - eaten.outerHeight()) / 2;
     var angle = -Math.atan2(deltay, -deltax);
 
 
     eater.css("transform", "rotate("+ String(angle) +"rad)");
-    eaten.css("transform", "translate(" + String(deltax / zoom) + "px, " +
-    String(deltay / zoom) + "px)  " + " scale(0)"
+    eaten.css("transform", "translate(" + String(deltax / Session.zoom) + "px, " +
+    String(deltay / Session.zoom) + "px)  " + " scale(0)"
   );
 
   window.setTimeout(function() {
@@ -272,11 +238,13 @@ Family.prototype.eat = function() {
     });
 
     replacements.forEach(function(data) {
-      data.egg.hatch(function() { data.egg.element.replaceWith(data.replacement.element); });
+      data.egg.hatch(function() {
+        data.egg.element.replaceWith(data.replacement.element);
+      });
     });
   }, 3000);
 }, hasToRename ? 1000 : 0);
-}
+};
 
 Family.prototype.associatedEggs = function(color) {
   if (!color) color = this.color;
@@ -286,19 +254,17 @@ Family.prototype.associatedEggs = function(color) {
   }).filter(function(egg) {
     return egg.color == color;
   });
-}
+};
 
 Family.prototype.clone = function() {
   var new_family = new Family(this.color, this.old_gator);
-  var family = this;
-
   this.row.element.children("egg, board-group").each(function() {
     var new_object = $(this).data("reference").clone();
     new_family.row.element.append(new_object.element);
   });
 
   return new_family;
-}
+};
 
 Family.checkAlpha = function(eater, eaten) {
   var eaterColors = eater.allColors();
@@ -309,15 +275,15 @@ Family.checkAlpha = function(eater, eaten) {
     if (color in eaterColors)
     {
       eatenColors[color].forEach(function(family) {
-        family.setColor(nextColor());
+        family.setColor(Session.nextColor());
         result = true;
       });
     }
-  })
+  });
   return result;
-}
+};
 
-Family.prototype.possibleEggs = function(color) {
+Family.prototype.possibleEggs = function() {
   var possibleColors = {};
   var current = this;
   while (current) {
@@ -327,9 +293,9 @@ Family.prototype.possibleEggs = function(color) {
     current = parent_element.data("reference");
   }
   return possibleColors;
-}
+};
 
-Family.prototype.getZoom = function(color) {
+Family.prototype.getZoom = function() {
   var zoom = 1.0;
   var current = this;
   while (current) {
@@ -339,17 +305,34 @@ Family.prototype.getZoom = function(color) {
     current = parent_element.data("reference");
   }
   return zoom;
-}
+};
 
 Family.prototype.setColor = function(color) {
   this.associatedEggs().forEach(function(egg) {
     egg.setColor(color);
-  })
+  });
   this.color = color;
   this.gator.setColor(color);
-}
+};
 
-function Row(family) {
+Family.import = function(data) {
+  var row = Row.import(data.children);
+  return (data.color === null) ?
+  new Family(null, true, row)
+  : new Family(data.color, false, row);
+};
+
+Family.prototype.export = function() {
+  return {
+    type: "family",
+    color: this.color,
+    children: this.row.export()
+  };
+};
+
+// ================================= ROW ==================================== //
+
+function Row() {
   var row = this;
 
   this.element = $(document.createElement("board-group")).addClass("row");
@@ -360,15 +343,12 @@ function Row(family) {
     items: "board-group.col, egg",
     placeholder: "drag-placeholder",
     connectWith: "board-group.row",
-    //forcePlaceholderSize: true
   });
 
   this.add_more = new Sprite("add-more.svg", "add-more");
   this.add_more.element.click(function(event) {
     $('.modal-overlay').addClass("shown");
-    var modal = $('#new-selector')
-    .empty()
-    .addClass("shown");
+    var modal = $('#new-selector').empty().addClass("shown");
 
     var oldGator = new Sprite("old-gator.svg", "gator");
     oldGator.element.addClass("short");
@@ -376,27 +356,29 @@ function Row(family) {
     oldGator.element.click(function() {
       var new_family = new Family(null, true);
       row.element.append(new_family.element);
-      modalClose();
+      Session.modalClose();
     });
 
     var rainbowGator = new Sprite("colored-gator.svg", "gator");
     rainbowGator.element.addClass("short");
     rainbowGator.element.appendTo(modal);
     rainbowGator.element.click(function() {
-      var new_family = new Family(nextColor());
+      var new_family = new Family(Session.nextColor());
       row.element.append(new_family.element);
-      modalClose();
+      Session.modalClose();
     });
 
-    if (family) {
+    var parent_family = row.parent();
+
+    if (parent_family) {
       $("<br>").appendTo(modal);
-      Object.keys(family.possibleEggs()).forEach(function(color) {
+      Object.keys(parent_family.possibleEggs()).forEach(function(color) {
         var selectEgg = new Egg(color);
         selectEgg.element.appendTo(modal);
         selectEgg.element.click(function() {
           var new_egg = new Egg(color);
           row.element.append(new_egg.element);
-          modalClose();
+          Session.modalClose();
         });
       });
     }
@@ -406,39 +388,26 @@ function Row(family) {
   });
 }
 
-Egg.prototype.export = function() {
-  return {
-    type: "egg", color: this.color
-  };
-}
 
-Family.prototype.export = function() {
-    return {
-      type: "family",
-      color: this.color,
-      children: this.row.export()
-    };
-}
+Row.prototype.allColors  = function() {
+  var result = {};
+  if (!this.old_gator) result[this.color]  = [ this ];
+  this.element.find("board-group.col, egg").each(function() {
+    var ref = $(this).data("reference");
+    if (ref.old_gator) return;
+    result[ref.color] = true;
+  });
 
-Row.prototype.export = function() {
-    return this.element.children("egg, board-group.col").toArray().map(function(element) {
-        return $(element).data("reference").export();
-      });
-}
+  return result;
+};
 
-Egg.import = function(data) {
-  return new Egg(data.color);
-}
+Row.prototype.parent = function() {
+  return this.element.parent().data("reference") || null;
+};
 
-Family.import = function(data) {
-  if (data.color !== null)
-    return new Family(data.color, false, function(family) { return Row.import(data.children, family) });
-  else
-    return new Family(null, true, function(family) { return Row.import(data.children, family) });
-}
+Row.import = function(data) {
+  var result = new Row();
 
-Row.import = function(data, parent) {
-  var result = new Row(parent);
   data.forEach(function(element) {
     if (element.type == "family") {
       result.element.append(Family.import(element).element);
@@ -449,10 +418,68 @@ Row.import = function(data, parent) {
     }
   });
   return result;
-}
+};
 
-window.addEventListener("beforeunload", function (event) {
-  localStorage.setItem("board", JSON.stringify(row.export()));
+Row.prototype.export = function() {
+  return this.element.children("egg, board-group.col").toArray().map(function(element) {
+    return $(element).data("reference").export();
+  });
+};
+
+// ========================================================================== //
+
+var Session = {
+  row: new Row(),
+  zoom: 1,
+  nextColor: function() {
+    var color = 0;
+    var colors = Session.row.allColors();
+    while (color in colors) { color++; }
+    return color;
+  },
+
+  attachRow: function() {
+    $("board").children().remove();
+    $("board").append(Session.row.element).append(Session.row.add_more.element.addClass("primary"));
+  },
+
+  init: function() {
+    var last_session = localStorage.getItem("board");
+    if (last_session) Session.row = Row.import(JSON.parse(last_session));
+
+    Session.attachRow();
+
+    window.addEventListener("beforeunload", function () {
+      localStorage.setItem("board", JSON.stringify(Session.row.export()));
+    });
+  },
+
+  modalClose: function () {
+    $('.modal-overlay').removeClass("shown");
+    $('.modal').removeClass("shown");
+  }
+};
+
+$('.modal-overlay').on("click", Session.modalClose);
+
+$("#toolbar-clear").click(function() {
+  Session.row.element.children().remove();
+});
+
+$("#toolbar-zoomin").click(function() {
+  Session.zoom *= 1.1;
+  if (Session.zoom > 1) Session.zoom = 1; // Gators don't scale well
+  $("board").css("transform", "translate(-50%, -50%) scale(" + String(Session.zoom) + ")");
+});
+
+$("#toolbar-save").click(function() {
+  var blob = new Blob([JSON.stringify(Session.row.export(), null, 2)], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, "board.json");
+});
+
+$("#toolbar-zoomout").click(function() {
+  Session.zoom /= 1.1;
+  $("board").css("transform", "translate(-50%, -50%) scale(" + String(Session.zoom) + ")");
 });
 
 $(document).on("dragover", function () { $("body").addClass('fileover'); return false; });
@@ -462,15 +489,15 @@ $(document).on("drop",  function (e) {
   e.preventDefault();
 
   var file = e.originalEvent.dataTransfer.files[0],
-      reader = new FileReader();
+  reader = new FileReader();
   reader.onload = function (event) {
-try {
-    row = Row.import(JSON.parse(event.target.result));
-    $("board").children().remove();
-    $("board").append(row.element).append(row.add_more.element.addClass("primary"));
-  } catch(e) {console.error(e);}
+    try {
+      Session.row = Row.import(JSON.parse(event.target.result));
+      Session.attachRow();
+    } catch(e) {
+      console.error(e);
+    }
   };
   reader.readAsText(file);
-
   return false;
 });
